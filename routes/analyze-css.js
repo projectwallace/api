2c923fda8a9bbc36abadfef35bd563148d1c10de
Bart Veneman <bartveneman@users.noreply.github.com>
2019-08-03 15:45:11 +0200

    add CORS to endpoints

--- routes/analyze-css.js
+++ routes/analyze-css.js
@@ -1,11 +1,12 @@
 import analyzeCss from "@projectwallace/css-analyzer";
+import cors from "micro-cors";
 import pkg from "../package.json";
 
-export default async (req, res) => {
+export default cors()(async (req, res) => {
   const stats = await analyzeCss(req.body);
   res.setHeader(
     "x-css-analyzer-version",
     pkg.dependencies["@projectwallace/css-analyzer"]
   );
   res.json(stats);
-};
+});
