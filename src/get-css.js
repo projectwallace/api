84a258c80990f4b4a87015eca5dc0ba4564e8775
Bart Veneman <bartveneman@users.noreply.github.com>
2020-01-14 21:31:07 +0100

    update dependencies, remove storeCss temporarily

--- src/get-css.js
+++ src/get-css.js
@@ -1,16 +1,12 @@
-import got from "got";
-import normalizeUrl from "normalize-url";
-import storeCss from "./store-css";
+import got from 'got'
+import normalizeUrl from 'normalize-url'
 
 export default async url => {
-  const normalizedUrl = normalizeUrl(url, {
-    stripProtocol: true,
-    stripWWW: true
-  });
-  const { body: css } = await got(
-    `https://extract-css.now.sh/${normalizedUrl}`
-  );
-  await storeCss({ filename: normalizedUrl, css });
+	const normalizedUrl = normalizeUrl(url, {
+		stripProtocol: true,
+		stripWWW: true
+	})
+	const { body: css } = await got(`https://extract-css.now.sh/${normalizedUrl}`)
 
-  return Promise.resolve(css);
-};
+	return Promise.resolve(css)
+}
