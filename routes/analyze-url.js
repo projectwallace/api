871e6d90e1e8ccb7d7e926458963174133c37073
Bart Veneman <bartveneman@users.noreply.github.com>
2020-02-13 22:22:26 +0100

    add error descriptions in case of syntax errors

--- routes/analyze-url.js
+++ routes/analyze-url.js
@@ -1,16 +1,28 @@
-import analyzeCss from "@projectwallace/css-analyzer";
-import cors from "micro-cors";
-import pkg from "../package.json";
-import getCss from "../src/get-css";
+import analyzeCss from '@projectwallace/css-analyzer'
+import cors from 'micro-cors'
+import pkg from '../package.json'
+import getCss from '../src/get-css'
 
 export default cors()(async (req, res) => {
-  const { url } = req.query;
-  const css = await getCss(url);
-  const stats = await analyzeCss(css);
+	try {
+		const { url } = req.query
+		const css = await getCss(url)
+		const stats = await analyzeCss(css)
+		res.setHeader(
+			'x-css-analyzer-version',
+			pkg.dependencies['@projectwallace/css-analyzer']
+		)
+		res.json(stats)
+	} catch (error) {
+		if (error.name === 'SyntaxError') {
+			return res.status(400).json({
+				name: error.name,
+				message: error.message
+			})
+		}
 
-  res.setHeader(
-    "x-css-analyzer-version",
-    pkg.dependencies["@projectwallace/css-analyzer"]
-  );
-  res.json(stats);
-});
+		return res.status(500).json({
+			message: 'Unexpected error'
+		})
+	}
+})
